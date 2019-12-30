d303bac23b37af285e4d852a7dc70e606c56655d
jvmahon <jvm33@columbia.edu>
2019-12-30 06:33:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ var yellow = chalk.yellow.bold;
 var cyan = chalk.cyan.bold;
 var magenta = chalk.magenta.bold;
 var assert = require('assert');
-			
+
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+
+// Checks for available update and returns an instance
+const notifier = updateNotifier({pkg, defer:false }) // Notify using the built-in convenience method
+notifier.notify();			
 
 var exports = module.exports;
 var globals = new function()
